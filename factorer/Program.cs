709c0e1988931b9ed7d1e3ed35f6f14d2b801e28
Britt Crozier <thompson.b.crozier@gmail.com>
2020-07-09 17:32:54 -0400

    optimize for smaller user input values

--- factorer/Program.cs
+++ factorer/Program.cs
@@ -7,12 +7,32 @@ namespace Factorer
     {
         static ulong Factor(ulong fct, ulong tbf) // fct is factor, tbf is to-be-factored user input
         {
-            ulong expn = 0, totalFct = 0; // exponent of fct, to be discovered and returned
+            ulong expn = 0, // exponent of each factor
+                totalFct = 0; // total factor count, to be discovered and returned
+            
+            if (tbf <= 4294967295) // optimize for smaller user input values
+            {
+                if (tbf <= 65535)
+                {
+                    if (tbf <= 255)
+                    {
+                        tbf = (byte)tbf;
+                        Console.WriteLine("Switched to byte!");
+                    } else {
+                        tbf = (ushort)tbf;
+                        Console.WriteLine("Switched to ushort!");
+                    }
+                } else {
+                    tbf = (uint)tbf;
+                    Console.WriteLine("Switched to uint!");
+                }
+            } else { Console.WriteLine("Sticking with ulong!"); }
+            
 
             do
             {
-                /* If fct is a factor of tbf, then reassign tbf to its divisor with fct; otherwise,
-                 * increment fct. */
+                /* If fct is a factor of tbf, then reassign tbf to its divisor with fct and increment
+                 * current exponent and total factor count; otherwise, increment fct and reset expn to 0. */
                 if (tbf % fct == 0)
                 {
                     Console.WriteLine("Just got factored down to:");
