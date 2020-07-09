b28b777944559ac34b570d616418108f43c90f69
Britt Crozier <thompson.b.crozier@gmail.com>
2020-07-09 12:19:47 -0400

    accept and format user input

---
+++ factorer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace factorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong userInput;
+
+            Console.WriteLine("Enter a positive integer up to 18,446,744,073,709,551,615 to be factored: ");
+
+            userInput = Convert.ToUInt64(Console.ReadLine());
+
+            Console.WriteLine(userInput);
+        }
+    }
+}
