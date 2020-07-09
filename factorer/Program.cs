927e9c07f9bb0f39ea86ddb39eefd0d7a33edc9f
Britt Crozier <thompson.b.crozier@gmail.com>
2020-07-09 16:57:58 -0400

    successfully factor ulong numbers (very slowly); next up: (1) arrange output object and (2) optimize variable types

--- factorer/Program.cs
+++ factorer/Program.cs
@@ -1,9 +1,35 @@
 ﻿using System;
 
-namespace factorer
+namespace Factorer
 {
+    static 
     class Program
     {
+        static ulong Factor(ulong fct, ulong tbf) // fct is factor, tbf is to-be-factored user input
+        {
+            ulong expn = 0, totalFct = 0; // exponent of fct, to be discovered and returned
+
+            do
+            {
+                /* If fct is a factor of tbf, then reassign tbf to its divisor with fct; otherwise,
+                 * increment fct. */
+                if (tbf % fct == 0)
+                {
+                    Console.WriteLine("Just got factored down to:");
+                    Console.WriteLine(tbf /= fct);
+                    expn++;
+                    totalFct++;
+                    Console.WriteLine(expn);
+                } else {
+                    Console.WriteLine("Next factor to try:");
+                    fct++;
+                    Console.WriteLine(fct);
+                    expn = 0;
+                };
+            } while (tbf > 1);
+
+            return totalFct;
+        }
         static void Main(string[] args)
         {
             ulong userInput;
@@ -13,6 +39,10 @@ namespace factorer
             userInput = Convert.ToUInt64(Console.ReadLine());
 
             Console.WriteLine(userInput);
+
+            // Factor(userInput);
+
+            Console.WriteLine(Factor(2, userInput));
         }
     }
 }
